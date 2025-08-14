65ef959d2105598f4154ac9f9aa3fe66aeedeb69
Dominykas Strazdas <dominykas.strazdas@ovgu.de>
2025-08-14 11:11:38 +0200

    Update camera publisher topic name to include node name

--- src/usb_cam_node.cpp
+++ src/usb_cam_node.cpp
@@ -45,8 +45,10 @@ UsbCamNode::UsbCamNode(const rclcpp::NodeOptions & node_options)
   m_image_msg(new sensor_msgs::msg::Image()),
   m_compressed_img_msg(nullptr),
   m_image_publisher(std::make_shared<image_transport::CameraPublisher>(
-      image_transport::create_camera_publisher(this, BASE_TOPIC_NAME,
-      rclcpp::QoS {100}.get_rmw_qos_profile()))),
+      image_transport::create_camera_publisher(
+        this,
+        this->get_name() + std::string("/") + BASE_TOPIC_NAME,
+        rclcpp::QoS {100}.get_rmw_qos_profile()))),
   m_compressed_image_publisher(nullptr),
   m_compressed_cam_info_publisher(nullptr),
   m_parameters(),
